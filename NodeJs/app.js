6b604d80a932e8250fdabfc0cc4df49b4079bf37
Lahiru Dhananjaya <codingchap@gmail.com>
2020-10-25 19:01:22 +0530

    Updates

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,18 +1,7 @@
 const express = require('express');
 const app = express();
-//var SerialPort = require("serialport");
 
 var port = 3000;
-
-var arduinoCOMPort = "COM3";
-
-// var arduinoSerialPort = new SerialPort(arduinoCOMPort, {  
-//  baudrate: 9600
-// });
-
-// arduinoSerialPort.on('open',function() {
-//   console.log('Serial Port ' + arduinoCOMPort + ' is opened.');
-// });
 
 app.get('/', function (req, res) {
 
@@ -23,21 +12,19 @@ app.get('/', function (req, res) {
 app.get('/:action', function (req, res) {
     debugger;
    var action = req.params.action;
-    
-    // if(action == 'led'){
-    //     //arduinoSerialPort.write("w");
-    //     console.write("w");
-    //     return res.send('Led light is on!');
-    // } 
-    // if(action == 'off') {
-    //     // arduinoSerialPort.write("t");
-    //     console.write("w");
-    //     return res.send("Led light is off!");
-    // }
-    console.log("w");
-    return res.send('Action1: ' + action);
- 
+   console.log(action);
+    if(action == 'led'){
+        console.log("Called to api /led");
+        return res.send('Led light is on!');
+    }else if(action == 'off') {
+        console.log("Called to api /off");
+        return res.send("Led light is off!");
+    }else{
+      console.log("Called to api route is not available");
+      return res.send('Invalid endpoint for ' + action);
+    }
 });
+
 
 app.listen(port, function () {
   console.log('Example app listening on port http://0.0.0.0:' + port + '!');
